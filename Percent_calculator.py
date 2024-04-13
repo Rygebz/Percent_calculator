0b40ccf7442039f03c9ad73b3f2e11f8b6778823
Rygebz <68190141+Rygebz@users.noreply.github.com>
2024-04-13 16:17:39 +0200

    Update Percent_calculator.py

--- Percent_calculator.py
+++ Percent_calculator.py
@@ -1,89 +1,61 @@
 def op1(num1, num2):
-	return number * percent / 100
+    return num1 * num2 / 100
+
 def op2(num1, num2):
-	return num1 / num2 * 100
+    return num1 / num2 * 100
+
 def op3(num1, num2):
-	return num1 / num2 * 100
+    return num1 / num2 * 100
+
 def op4(num):
-	return num / 100
-op = 1
-chr(op)
+    return num / 100
 
-print(" 1. percent of a number \n \
-2. percent of is number \n \
-3. number is percent of \n \
-4. Percent of a number \n")
+operations = {
+    '1': op1,
+    '2': op2,
+    '3': op3,
+    '4': op4
+}
 
-choice = input("Enter opertation: ")
-while(op != 'e'):
-	if choice == '1':
-		#op1
-		percent = int(input("Enter percent: "))
-		number = int(input("Enter number: "))
-		result = print(percent, "%", "for", number, "=", op1(percent, number))
-		print("Are u want to continue, y or n?")
-		choice = input("Enter opertation: ")
-		while True:
-			if choice == 'y': 
-				percent = int(input("Enter percent: "))
-				number = int(input("Enter number: "))
-				result = print(percent, "%", "for", number, "=", op1(percent, number))
-				print("Are u want to continue, Y or N ?")
-				choice = input("Enter opertation: ")
-			break	
-		if choice == 'n':
-			print(" 1. percentage of a number, \n 2. What percent of is, \n 3. What number of percent is, \n 4. Percent of a number,")
-			choice = input("Enter operation: ")
-	elif choice == '2':
-		#op2
-		num1 = int(input("Enter first num: "))
-		num2 = int(input("Enter second num: "))
-		result = print("What percent of", num1, "is", num2, "=", op2(num1, num2), "%")
-		print("Are u want to continue, y or n?")
-		choice = input("Enter opertation: ")
-		while True:
-			if choice == 'y':
-				percent = int(input("Enter percent: "))
-				number = int(input("Enter number: "))
-				result = print(percent, "%", "for", number, "=", op1(percent, number))
-				print("Are u want to continue, Y or N ?")
-				choice = input("Enter opertation: ")
-			break	
-		if choice == 'n':
-			print(" 1. percentage of a number, \n 2. What percent of is, \n 3. What number of percent is, \n 4. Percent of a number,")
-			choice = input("Enter operation: ")
-	elif choice == '3':
-		#op3
-		num = int(input("Enter number: "))
-		percent = int(input("Enter percent: "))
-		result = print(num, "is", percent, "=", op3(num, percent))
-		print("Are u want to continue, y or n?")
-		choice = input("Enter opertation: ")
-		while True:
-			if choice == 'y': 
-				percent = int(input("Enter percent: "))
-				number = int(input("Enter number: "))
-				result = print(percent, "%", "for", number, "=", op1(percent, number))
-				print("Are u want to continue, Y or N ?")
-				choice = input("Enter opertation: ")
-			break	
-		if choice == 'n':
-			print(" 1. percentage of a number, \n 2. What percent of is, \n 3. What number of percent is, \n 4. Percent of a number,")
-			choice = input("Enter operation: ")
-	elif choice == '4':
-		#op4
-		num = int(input("Enter number: "))
-		result = print("Here is percent of this number ->", op4(num))
-		print("Are u want to continue, y or n?")
-		choice = input("Enter opertation: ")
-		while True:
-			if choice == 'y': 
-				percent = int(input("Enter percent: "))
-				number = int(input("Enter number: "))
-				result = print(percent, "%", "for", number, "=", op1(percent, number))
-				print("Are u want to continue, Y or N ?")
-				choice = input("Enter opertation: ")
-			break	
-		if choice == 'n':
-			print(" 1. percentage of a number, \n 2. What percent of is, \n 3. What number of percent is, \n 4. Percent of a number,")
-			choice = input("Enter operation: ")	
+def get_operation_choice():
+    print("1. percent of a number\n \
+    2. percent of is number\n \
+    3. number is percent of\n \
+    4. Percent of a number\n")
+    return input("Enter operation: ")
+
+def perform_operation(choice):
+    if choice in operations:
+        operation = operations[choice]
+
+        if choice == '1':
+            percent = int(input("Enter percent: "))
+            number = int(input("Enter number: "))
+            result = operation(percent, number)
+            print(percent, "% for", number, "=", result)
+
+        elif choice == '2':
+            num1 = int(input("Enter first num: "))
+            num2 = int(input("Enter second num: "))
+            result = operation(num1, num2)
+            print("What percent of", num1, "is", num2, "=", result, "%")
+
+        elif choice == '3':
+            num = int(input("Enter number: "))
+            percent = int(input("Enter percent: "))
+            result = operation(num, percent)
+            print(num, "is", percent, "=", result)
+
+        elif choice == '4':
+            num = int(input("Enter number: "))
+            result = operation(num)
+            print("Result: ", result)
+
+def main():
+    choice = get_operation_choice()
+    while choice != 'e':
+        perform_operation(choice)
+        choice = get_operation_choice()
+
+if __name__ == "__main__":
+    main()
